8e9b075ac91a30713a5f92ff8a1bec72acec6948
Rajesh Jadhav <jadhavrajesh584@gmail.com>
2026-09-16 12:57:23 +0530

    Fix chart  for production frontend

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,19 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import './styles.css';
-import App from './App';
-import { AppProvider } from './context/AppContext';
-import { Chart, ArcElement, Tooltip, Legend, CategoryScale, LinearScale, PointElement, LineElement, BarElement, Filler } from 'chart.js';
-
-Chart.register(ArcElement, Tooltip, Legend, CategoryScale, LinearScale, PointElement, LineElement, BarElement, Filler);
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "./styles.css";
+import App from "./App";
+import { AppProvider } from "./context/AppContext";
+import "./chartSetup";
+Chart.register(
+  ArcElement,
+  Tooltip,
+  Legend,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  BarElement,
+  Filler,
+);
 Chart.defaults.font.family = '"Segoe UI", system-ui, sans-serif';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AppProvider>
       <App />
     </AppProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
